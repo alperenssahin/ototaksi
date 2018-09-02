e1d5d3a4123be0632c142e89191152e94ba955ba
lebigmac <allperensahin@gmail.com>
2018-09-02 17:22:39 +0300

    servo init

--- motor.py
+++ motor.py
@@ -2,16 +2,7 @@ from picar.SunFounder_PCA9685 import PCA9685
 import sys
 import time
 
-pwm = PCA9685.PWM() # address is 0x40 by default
+pwm = PCA9685.PWM(1 , 0x40) # address is 0x40 by default
 pwm.frequency = 60
-
-while True:
-        for i in range(0, 4095, 10):
-                print i
-                pwm.write(4,0, i)
-                time.sleep(0.0001)
-        for i in range(4095, -1, -10):
-                print i
-                pwm.write(5,0, i)
-                time.sleep(0.0001)
-        sys.exit()
+pwm.write(4,0,4000)
+sys.exit()
