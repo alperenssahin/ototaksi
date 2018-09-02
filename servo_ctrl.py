2cc8c85d7d2f706eb01cca034157b96a995886a9
lebigmac <allperensahin@gmail.com>
2018-09-02 15:23:37 +0300

    servo init

--- servo_ctrl.py
+++ servo_ctrl.py
@@ -1,11 +1,13 @@
 from picar.SunFounder_PCA9685 import Servo
 import sys
+import time
 # class servo(Servo):
 #     def __init__(self,pin_no):
 #         self.servo = Servo.servo(pin_no)
 
 
-srv = Servo.Servo(1)
+srv = Servo.Servo(0)
 srv.setup()
 srv.write(int(sys.argv[1]))
+time.sleep(0.1)
 sys.exit()
